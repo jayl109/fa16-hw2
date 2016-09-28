b5c0c2e5a4d9687aad3308e533f9de9333310874
Jason Lum <jthemathfreak@gmail.com>
2016-09-27 17:30:24 -0700

    image doesnt work but everything else does

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,11 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+
   def initialize(param)
+  	@variable=param
+
+  end
+  def bar(param1, param2)
+
+  	return param1.to_s+@variable+param2[:sat].to_s
   end
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,25 +4,34 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname=name[0,4]
   end
 
   def nickname
-    # YOUR IMPLEMENTATION HERE
-    'nickname'
+    return @nickname
+    
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    return 2016-age.to_i
   end
 
   def introduction
-    # YOUR IMPLEMENTATION HERE
-    'introduce'
+    return @name+" is "+@age+" years old"
   end
 
   def fib_number
-    # YOUR IMPLMENTATION HERE
-    0
+      if @age==1.to_s or @age==2.to_s
+        return 1
+      end
+      @num1=1
+      @num2=1
+      @count=@age.to_i-2
+      while @count>0 do 
+        @num2, @num1=@num2+@num1, @num2
+        @count=@count-1
+      end
+      return @num2
   end
 end
